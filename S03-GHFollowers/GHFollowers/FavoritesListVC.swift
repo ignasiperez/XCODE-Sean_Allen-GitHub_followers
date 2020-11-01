5dc84e2c0c80a352e8ce91af26ffa42cb9935b6e
Ignasi  Perez <perezignasi@gmail.com>
2020-11-01 08:27:36 +0100

    V08. Search Screen UI (Programmatic)

---
+++ S03-GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteListVC.swift
+//  GHFollowers
+//
+//  Created by ignasiperez.com on 25/10/2020.
+//  Copyright © 2020 Ignasi Perez-Valls. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      view.backgroundColor = .systemBlue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ S03-GHFollowers/GHFollowers/SearchVC.swift
@@ -0,0 +1,141 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by ignasiperez.com on 24/10/2020.
+//  Copyright © 2020 Ignasi Perez-Valls. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+  
+  let logoImageView       = UIImageView()
+  let usernameTextField   = GFTextField()
+  let callToActionButton  = GFButton(backgroundColor: .systemGreen,
+                                     title: "Get Followers")
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    configureLogoImageView()
+    configureTextField()
+    configureCallToActionButton()
+  }
+  
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.isNavigationBarHidden = true
+  }
+  
+  
+  func configureLogoImageView() {
+    view.addSubview(logoImageView)
+    logoImageView.translatesAutoresizingMaskIntoConstraints = false
+    logoImageView.image = UIImage(named: "gh-logo")!
+    
+    NSLayoutConstraint.activate([
+      logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                         constant: 80),
+      logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      logoImageView.heightAnchor.constraint(equalToConstant: 200),
+      logoImageView.widthAnchor.constraint(equalToConstant: 200)
+    ])
+  }
+  
+  
+  func configureTextField() {
+    view.addSubview(usernameTextField)
+    
+    NSLayoutConstraint.activate([
+      usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor,
+                                             constant: 48),
+      usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+                                                 constant: 50),
+      usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                                  constant: -50),
+      usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+    ])
+  }
+  
+  
+  func configureCallToActionButton() {
+    view.addSubview(callToActionButton)
+    
+    NSLayoutConstraint.activate([
+      callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+      callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+      callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+      callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+    ])
+  }
+}
+
+//import UIKit
+//
+//class SearchVC: UIViewController {
+//  let logoImageView = UIImageView()
+//  let usernameTextField = GFTextField()
+//  let callToActionButton = GFButton(backgroundColor: .systemGreen,
+//                                    title: "Get Followers")
+//
+//
+//  override func viewDidLoad() {
+//    super.viewDidLoad()
+//
+//    view.backgroundColor = .systemBackground
+//    configureLogoImageView()
+//    configureTextField()
+//  }
+//
+//
+//  override func viewWillAppear(_ animated: Bool) {
+//    super.viewWillAppear(animated)
+//
+//    navigationController?.isNavigationBarHidden = true
+//  }
+//
+//
+//  private func configureLogoImageView() {
+//    view.addSubview(logoImageView)
+//
+//    logoImageView.translatesAutoresizingMaskIntoConstraints = false
+//    logoImageView.image = UIImage(named: "gh-logo")
+//
+//    NSLayoutConstraint.activate([
+//      logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+//                                         constant: 80),
+//      logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//      logoImageView.heightAnchor.constraint(equalToConstant: 200),
+//      logoImageView.widthAnchor.constraint(equalToConstant: 200)
+//    ])
+//  }
+//
+//
+//  private func configureTextField() {
+//    view.addSubview(usernameTextField)
+//
+//    NSLayoutConstraint.activate([
+//      usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor,
+//                                             constant: 48),
+//      usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+//                                                 constant: 50),
+//      usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+//                                                  constant: -50),
+//      usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+//    ])
+//  }
+//
+//  /*
+//   // MARK: - Navigation
+//
+//   // In a storyboard-based application, you will often want to do a little preparation before navigation
+//   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//   // Get the new view controller using segue.destination.
+//   // Pass the selected object to the new view controller.
+//   }
+//   */
+//
+//}
