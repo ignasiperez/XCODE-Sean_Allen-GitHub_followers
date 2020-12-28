c35faf7d9c7021ec175559a26be4c0d3e0c12845
Ignasi  Perez <perezignasi@gmail.com>
2020-12-28 19:36:02 +0100

    XE-04 App_Navigation_Setup–UITabBarController_&_UINavigationController

---
+++ XE-04-GHFollowers/GHFollowers/FavoriteListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteListVC.swift
+//  GHFollowers
+//
+//  Created by ignasiperez.com on 24/12/2020.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemBlue
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
